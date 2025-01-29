2a38a2a1bee74608962f25aabb5f29bc36e24346
rimu-7 <rrimu99@gmail.com>
2025-01-29 22:34:14 +0800

    main

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Navbar from '../Pages/Shared/Navbar/Components/LargeScreen';
 
 
 const Main = () => {
     return (
         <div className='bg-[#212121]'>
             <div className=""></div>
+            {/* <Navbar/> */}
             <Navbar/>
 
             <div className=''>
