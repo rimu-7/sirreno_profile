f41257ff8363e140821306d405ddbd76393a5031
rimu-7 <rrimu99@gmail.com>
2025-01-29 22:35:20 +0800

    Main

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Pages/Shared/Navbar/Components/LargeScreen';
+import Navbar from '../Pages/Shared/Navbar/Components/Navbar';
 
 
 const Main = () => {
